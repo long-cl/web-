9fb5b8e318ef401511c9ed1bcac040028198b38f
longcl <1281547662@qq.com>
2020-12-01 12:32:46 +0800

    1

--- src/lib/tool/str_add.js
+++ src/lib/tool/str_add.js
@@ -1,9 +1,8 @@
-var str = `37 7 2 1 11 `
+var str = `35 31 32 33 32 37 21 23 23 28 44 15 40 10 31 40 19 30 41 62`
 
 function strAdd(str){
   var sum = 0;
   var arr = str.trim().split(' ');
-  console.log(arr.length);
   for(var i= 0; i<arr.length; i++){
     sum += Number(arr[i])
   }
